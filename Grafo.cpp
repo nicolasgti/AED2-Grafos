a2af7c3503104214ec556762d69fd7d9cc512f80
nicolasgti <31924735+nicolasgti@users.noreply.github.com>
2019-09-17 11:01:15 -0400

    Project att
    funcional

--- Grafo.cpp
+++ Grafo.cpp
@@ -10,97 +10,190 @@ using namespace std;
 typedef int Vertex;
 
 
-template <typename Tipo>
-class No{
+using namespace std;
+
+// classe No
+class No
+{
 private:
-	Tipo item;
-	No<Tipo> *prox;
+	int v;
+	No* prox;
 public:
-	No(){
-		prox = NULL;
+	No(int v) // construtor
+	{
+		this->v = v;
+		this->prox = NULL;
 	}
-	No(Tipo){
-		this->item = item;
+
+	int obterValor() // obtém o valor
+	{
+		return v;
 	}
-	No<Tipo>* getProx(){
+
+	No* obterProx() // obtém o próximo No
+	{
 		return prox;
 	}
-	void setProx(No<Tipo>* prox){
-		this->prox = prox;
-	}
-	Tipo getItem(){
-		return item;
-	}
-	void setItem(Tipo item){
-		this->item=item;
-	}
 
+	void setProx(No* p) // seta o próximo No
+	{
+		prox = p;
+	}
 };
 
-template <typename Tipo>
-class Lista{
+// classe Lista
+class Lista
+{
 private:
-	No<Tipo>* prim;
-	No<Tipo>* ult;
-	bool vazia();
+	No* cabeca; // primeiro elemento
+	No* cauda; // último elemento
+
 public:
-	Lista(){
-		cria();
-	};
-	
-	void cria();
-	void insereObj(Tipo);
-	void removePrim();
-	void imprime();
-	No<Tipo>* busca(Lista l, char c);
-	No<Tipo>* buscaObj(string);
-	void imprimeObj();
+	Lista()
+	{
+		// se não passar elemento, então cabeca e cauda são NULL
+		cabeca = NULL;
+		cauda = NULL;
+	}
+
+	Lista(int v)
+	{
+		// se passar elemento, então cria novo No
+		cabeca = new No(v);
+		cauda = cabeca;
+	}
+
+	virtual ~Lista() // destrutor
+	{
+		delete cabeca;
+	}
+
+	void mostrar() // mostra todos os elementos da lista
+	{
+		cout << "\nImprimindo todos os elementos...\n";
+		No* c = cabeca;
+
+		if(vazia())
+			cout << "A lista NAO possui elementos!!\n";
+		else
+		{
+			while(c) // percorre a lista
+			{
+				cout << c->obterValor() << endl;
+				c = c->obterProx();
+			}
+			cout << endl;
+		}
+	}
+
+	bool vazia() // verifica se a lista está vazia
+	{
+		return (cabeca == NULL);
+	}
+
+	// insere no início (semelhante a push_front da list)
+	void inserir_inicio(int v)
+	{
+		No* novo_no = new No(v);
+
+		if(vazia())
+		{
+			cabeca = novo_no;
+			cauda = novo_no;
+		}
+		else
+		{
+			novo_no->setProx(cabeca);
+			cabeca = novo_no;
+		}
+	}
+
+	// insere no final (semelhante a push_back da list)
+	void inserir_final(int v)
+	{
+		No* novo_no = new No(v);
+
+		if(vazia())
+		{
+			cabeca = novo_no;
+			cauda = novo_no;
+		}
+		else
+		{
+			cauda->setProx(novo_no);
+			cauda = novo_no;
+		}
+	}
+
+	// retorna o tamanho da lista
+	int tamanho()
+	{
+		if(vazia()) // se for vazia, entã retorna 0
+			return 0;
+
+		No* c = cabeca;
+		int tam = 0;
+		
+		// percorre a lista
+		do
+		{
+			c = c->obterProx();
+			tam++;
+		}
+		while(c);
+		
+		return tam;
+	}
+
+	// verifica se um elemento existe na lista
+	bool existe(int v)
+	{
+		No* c = cabeca;
+		
+		while(c)
+		{
+			if(c->obterValor() == v)
+				return true;
+			c = c->obterProx();
+		}
+		return false;
+	}
+
+	// remove da lista, remoção do final (semelhante a pop_back da list)
+	void remover()
+	{
+		if(!vazia())
+		{
+			// se houver só 1 elemento
+			if(cabeca->obterProx() == NULL)
+				cabeca = NULL;
+			else if(cabeca->obterProx()->obterProx() == NULL) // 2 elementos
+				cabeca->setProx(NULL);
+			else // > 2 elementos
+			{
+				No* ant_ant = cabeca;
+				No* ant = cabeca->obterProx();
+				No* corrente = cabeca->obterProx()->obterProx();
+
+				while(corrente)
+				{
+					No* aux = ant;
+					ant = corrente;
+					ant_ant = aux;
+					corrente = corrente->obterProx();
+				}
+				delete ant_ant->obterProx(); // libera memória
+				ant_ant->setProx(NULL); // seta o prox como NULL
+				cauda = ant_ant; // atualiza a cauda
+			}
+		}
+	}
 };
-
-template <typename Tipo>
-void Lista<Tipo>::cria(){
-	prim = new No<Tipo>;
-	prim->setProx(NULL);
-	prim->setL(NULL);
-	ult = prim;
-}
-template <typename Tipo>
-bool Lista<Tipo>::vazia(){
-	return prim == ult;
-}
-
-template <typename Tipo>
-void Lista<Tipo>::insereObj(Tipo i){
-	ult->setProx(new No<Tipo>());
-	ult = ult->getProx();
-	ult->setProx(NULL);
-	ult->setItem(i);
-}
-
-template <typename Tipo>
-void Lista<Tipo>::imprime(){
-	No<Tipo> *p = prim->getProx();
-	while(p != NULL){
-		cout << p->getL().getLetra();
-		p = p->getProx();
-	}
-	cout << endl;
-}
-
-template <typename Tipo>
-void Lista<Tipo>::imprimeObj(){
-	No<Tipo> *p = prim->prox;
-	while(p != NULL){
-		cout << "(" << p->item.codigo << "," << p->item.letra <<")";
-		p = p->prox;
-		cout << endl;
-	}
-}
 
 
 class Graph
 { // Não-direcionado
-    Lista<Vertex> *adj;
+    Lista *adj;
     int n, m; // ordem e tamanho
     void destroy();
 
@@ -122,35 +215,31 @@ void Graph::initialize(int n)
     if (this->n != 0)
         destroy();
     this->n = n;
-    adj = new Lista<Vertex>[n + 1]; // Vetor usa
+    adj = new Lista[n + 1]; // Vetor usa
     // células de 1..n
 }
 
 void Graph::insertEdge(Vertex u, Vertex v)
 {    // chave = vértice
-    adj[u].insereObj({v}); // Insere na lista
-    adj[v].insereObj({u}); // Insere na lista
+    adj[u].inserir_final({v}); // Insere na lista
+    adj[v].inserir_final({u}); // Insere na lista
     m++;
 }
 
 void Graph::print()
 {
-    for (int i = 1; i <= n; i++)
-    {
-        cout << "v[" << i << "] = ";
-        adj[i].imprimeObj();
-    }
+    Graph.Lista.mostrar();
 };
 
-// void Graph::destroy()
-// {
-//     for (int i = 0; i <= n; i++)
-//     {
-//         adj[i].destroy(); // destroi lista
-//     }
-//     delete (adj);
-//     n = m = 0;
-// }
+void Graph::destroy()
+{
+    for (int i = 0; i <= n; i++)
+    {
+        adj[i].remover(); // destroi lista
+    }
+    delete (adj);
+    n = m = 0;
+}
 
 // Função auxiliar
 void testaGrafo(Graph &g)
